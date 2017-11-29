37dcf6d707da8d1650390cfbb04a0da20904488f
ajbraus <ajbraus@gmail.com>
2017-11-29 15:25:41 -0800

    adding petunia and fixing edit

--- json/pets.js
+++ json/pets.js
@@ -1,21 +1,21 @@
 module.exports = [
     {
-        name: "Rex",
+        name: "Petunia",
         species: "Rottweiler",
-        birthday: "2008-11-11",
+        birthday: "20017-11-11",
         favoriteFood: "Chicken",
         picUrl: "http://www.dogbreedslist.info/uploads/allimg/dog-pictures/Rottweiler-3.jpg",
         picUrlSq: "http://www.dogbreedplus.com/dog_breeds/images/cute-rottweiler-puppy.jpg",
         description: "Rex is a dog and he's a good dog who loves to play and hang out with his owners. He also likes to nap and enjoys eating dog food"
     },
     {
-        name: "Rex",
-        species: "Rottweiler",
-        birthday: "2008-11-11",
-        favoriteFood: "Chicken",
-        picUrl: "http://www.dogbreedslist.info/uploads/allimg/dog-pictures/Rottweiler-3.jpg",
-        picUrlSq: "http://www.dogbreedplus.com/dog_breeds/images/cute-rottweiler-puppy.jpg",
-        description: "Rex is a dog and he's a good dog who loves to play and hang out with his owners. He also likes to nap and enjoys eating dog food"
+        name: "Nuggie",
+        species: "Fighting Fish",
+        birthday: "20015-11-11",
+        favoriteFood: "Fish Food",
+        picUrl: "http://www.woaw.org.au/wp-content/images/uploads/siamese-fighting-fish.jpg",
+        picUrlSq: "https://static.turbosquid.com/Preview/2014/07/07__22_22_55/color_version.jpg1d99dbda-18a4-450e-ac86-18638e2da3b2Original.jpg",
+        description: "Petunia is a fighting fish, and boy is she a killer. Killed three grown men in hand-to-finn combat so far. Don't get too close!"
     },
     {
         name: "Rex",
