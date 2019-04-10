bb70e65a3e22524d8e185b26ddffc7422dcd7f48
Adam Braus <ajbraus@gmail.com>
2019-04-10 14:30:31 -0700

    Update pets.js

--- seeds/pets.js
+++ seeds/pets.js
@@ -5,6 +5,7 @@ module.exports = [
         "birthday": "20017-11-11",
         "favoriteFood": "Chicken",
         "picUrl": "http://www.dogbreedslist.info/uploads/allimg/dog-pictures/Rottweiler-3.jpg",
+        "price": 9.99,
         "picUrlSq": "http://www.dogbreedplus.com/dog_breeds/images/cute-rottweiler-puppy.jpg",
         "description": "Rex is a dog and he's a good dog who loves to play and hang out with his owners. He also likes to nap and enjoys eating dog food"
     },
@@ -14,6 +15,7 @@ module.exports = [
         "birthday": "2008-11-11",
         "favoriteFood": "Liver",
         "picUrl": "http://www.gpamass.com/s/img/emotionheader713297504.jpg",
+        "price": 9.99,
         "picUrlSq": "https://www.collinsdictionary.com/images/thumb/greyhound_21701074_250.jpg",
         "description": "Fido is a dog and he's a good dog who loves to play and hang out with his owners. He also likes to nap and enjoys eating dog food"
     },
@@ -23,6 +25,7 @@ module.exports = [
         "birthday": "2008-11-11",
         "favoriteFood": "Beef",
         "picUrl": "https://www.pocketpitbull.com/wp-content/uploads/2017/03/blue-staffy-pitbull-370x208.jpg",
+        "price": 9.99,
         "picUrlSq": "http://www.vrcpitbull.com/wp-content/uploads/2011/02/VAMPVAMP%E2%80%A2.jpg",
         "description": "Rolfe is a dog and he's a good dog who loves to play and hang out with his owners. He also likes to nap and enjoys eating dog food"
     },
@@ -41,6 +44,7 @@ module.exports = [
         "birthday": "2008-11-11",
         "favoriteFood": "Chicken",
         "picUrl": "https://i.ytimg.com/vi/-57vwuw_aY8/maxresdefault.jpg",
+        "price": 9.99,
         "picUrlSq": "http://www.caninest.com/images/black-poodle.jpg",
         "description": "Mr. Fluffles is a dog and he's a good dog who loves to play and hang out with his owners. He also likes to nap and enjoys eating dog food"
     },
@@ -50,6 +54,7 @@ module.exports = [
         "birthday": "2008-11-11",
         "favoriteFood": "Pork",
         "picUrl": "http://via.placeholder.com/350x150",
+        "price": 9.99,
         "picUrlSq": "http://via.placeholder.com/250x250",
         "description": "Santa's Little Helper is a dog and he's a good dog who loves to play and hang out with his owners. He also likes to nap and enjoys eating dog food"
     },
@@ -59,6 +64,7 @@ module.exports = [
         "birthday": "2008-11-11",
         "favoriteFood": "Cats",
         "picUrl": "http://via.placeholder.com/350x150",
+        "price": 59.99,
         "picUrlSq": "http://via.placeholder.com/250x250",
         "description": "Alf is an alien and he's a good alien who loves to play and hang out with his owners. He also likes to nap and enjoys eating cats"
     },
@@ -68,6 +74,7 @@ module.exports = [
         "birthday": "2008-11-11",
         "favoriteFood": "Dragon",
         "picUrl": "http://via.placeholder.com/350x150",
+        "price": 9.99,
         "picUrlSq": "http://via.placeholder.com/250x250",
         "description": "Bubba is a dog and he's a good dog who loves to play and hang out with his owners. He also likes to nap and enjoys eating dog food"
     }
