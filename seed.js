b8a51d14b02c0bfd7ef29a560849807fef11a7e7
ysawiris <youssef.sawiris@students.dominican.edu>
2020-10-26 15:42:19 -0700

    cloned and seeded data

--- seed.js
+++ seed.js
@@ -1,5 +1,5 @@
-process.env.NODE_ENV = "dev"
 module.exports = {
-	"dev": "localhost/petes-pets"
-	// "prod": "localhost/PROD_DB_NAME"
+	"undefined": "localhost:27017/petes-pets",
+	"dev": "localhost:27017/petes-pets",
+	"prod": "localhost:27017/petes-pets"
 }
